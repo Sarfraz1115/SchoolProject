454e91e764be0ea9c09dcd4eeb3f07d5397b081e
Sarfraz1115 <syedsarfraz907@gmail.com>
2026-01-10 11:27:59 +0530

    about changes

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Herosection from '../components/home/Herosection'
 import Gallery from '../components/home/Gallery'
-import Aboutsection from '../components/home/aboutsection'
+import Aboutsection from '../components/home/Aboutsection'
 
 const Home = () => {
   return (
